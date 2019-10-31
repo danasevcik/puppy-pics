dc03fd0892c6e91b4ef8dd4515c9fadc29ffadc1
Dana Sevcik <danasevcik@hotmail.com>
2019-10-31 17:12:23 -0400

    updates state in image container and adds comments

--- src/containers/ImageContainer.js
+++ src/containers/ImageContainer.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 
 class ImageContainer extends Component {
 
+  // state to hold currently shown breed and current photourl
   state = {
-    breed: this.props.chosenBreed,
-    photoUrl: null
+    currentBreed: this.props.chosenBreed,
+    currentPhotoUrl: null
   }
 
+  // fetch to get one randomized photo based on clicked breed
+  // update local state to reflect current breed and url
+  // scroll pagee to the top
   getPhoto = () => {
     fetch(`https://dog.ceo/api/breed/${this.props.chosenBreed}/images/random`)
     .then(resp => resp.json())
     .then(image => {
       this.setState({
-        photoUrl: image.message,
-        breed: this.props.chosenBreed
+        currentPhotoUrl: image.message,
+        currentBreed: this.props.chosenBreed
       })
     })
     window.scroll({
@@ -23,17 +27,21 @@ class ImageContainer extends Component {
     });
   }
 
+  // called from app
+  // if chosen breed exists in props and the new chosen is updated from the current breed shown
+  // call get photo 
   render() {
-    if (this.props.chosenBreed && (this.props.chosenBreed !== this.state.breed)) {
+    if (this.props.chosenBreed && (this.props.chosenBreed !== this.state.currentBreed)) {
+      console.log('here');
       this.getPhoto()
     }
     return (
       <div>
-        {this.state.photoUrl &&
+        {this.state.currentPhotoUrl &&
           <h1>{this.props.chosenBreed[0].toUpperCase() + this.props.chosenBreed.slice(1)}</h1>
         }
-        {this.state.photoUrl &&
-          <img src={this.state.photoUrl} alt='puppy based on clicked breed' id='puppy-img'/>
+        {this.state.currentPhotoUrl &&
+          <img src={this.state.currentPhotoUrl} alt='puppy based on clicked breed' id='puppy-img'/>
         }
       </div>
     )
