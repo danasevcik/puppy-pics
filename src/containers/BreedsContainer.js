6013d6b6e746159fd777e647a439d362ffae635b
Dana Sevcik <danasevcik@hotmail.com>
2019-10-31 17:03:09 -0400

    comments in breeds container

--- src/containers/BreedsContainer.js
+++ src/containers/BreedsContainer.js
@@ -3,23 +3,30 @@ import BreedCard from '../components/BreedCard'
 
 class BreedsContainer extends Component {
 
+  // state to hold all breeds
   state = {
     breeds: null
   }
 
+  // fetch to get all breeds
+  // call format breeds to format the json response
   componentDidMount() {
     fetch('https://dog.ceo/api/breeds/list/all')
     .then(resp => resp.json())
     .then(breeds => {
-      this.getBreeds(breeds.message)
+      this.formatBreeds(breeds.message)
     })
   }
 
-  getBreeds(breeds) {
+  // get keys from the breeds object
+  // set state with list of breeds
+  formatBreeds(breeds) {
     let breedsArray = Object.keys(breeds)
     this.setState({breeds: breedsArray})
   }
 
+  // map through breeds in state and print card for each breed
+  // pass the breed, key and click handler
   printCards() {
     let keyNum = 0
     return this.state.breeds.map(breed => {
@@ -28,11 +35,14 @@ class BreedsContainer extends Component {
     })
   }
 
+  // rendered from App
+  // if the fetch has already completed and breeds is truthy in state
+  // call print cards
   render() {
     return (
       <div>
-      <h1>Please choose a breed!</h1>
-      {this.state.breeds && this.printCards()}
+        <h1>Please choose a breed!</h1>
+        {this.state.breeds && this.printCards()}
       </div>
     )
 
