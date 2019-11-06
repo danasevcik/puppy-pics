8767c1c3425ac26e24882e4b63fed91cb2e81ebd
Dana Sevcik <danasevcik@hotmail.com>
2019-11-06 12:03:35 -0500

    adds breed search test

---
+++ src/tests/BreedSearch.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import 'jest-enzyme';
+import BreedSearch from '../components/BreedSearch';
+
+it('renders breed search html', () => {
+  const wrapper = shallow(<BreedSearch />);
+  const onChange = jest.fn();
+  const onClick = jest.fn();
+  const div = (
+    <div>
+      <input
+        type='name'
+        id='search-field'
+        value=''
+        placeholder='Search For a Breed'
+        onChange={onChange}/>
+      <br/>
+      <input
+        type='submit'
+        id='all-breeds-button'
+        value='See All Breeds'
+        onClick={onClick}
+      />
+    </div>);
+  expect(wrapper).toExist(div);
+});
